c9a1ff86bfa82abec94ea7add23e58ff368c19d6
Rustam <rustamg23@outlook.com>
2024-03-22 04:35:56 +0400

    dd

--- _deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/rustam/Documents/GitHub/istu_test_2024_2/_deps/googletest-src/googletest
-# Build directory: /Users/rustam/Documents/GitHub/istu_test_2024_2/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
